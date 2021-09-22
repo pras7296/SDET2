7bf1d58a2c24feda02d1d12b054d00daf4dc39c6
pras7296 <vijaysantosh007@gmail.com>
2021-09-22 11:10:10 +0530

    First commit

---
+++ ComcastSeleniumFrm/src/test/java/com/comcast/contact/ContactTest.java
@@ -0,0 +1,21 @@
+package com.comcast.contact;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void CreateContactTest(){
+		System.out.println("execute create contact");
+	}
+	
+	@Test
+	public void CreateContactWithOrgTest(){
+		System.out.println("Execute CreateContactWithOrg");
+	}
+	
+	@Test
+	public void DeleteContactTest() {
+		System.out.println("execute Delete contact");
+	}
+
+}
