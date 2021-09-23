fae63fe34457ecc0ca13a470a7fb5d29ab906f0d
pras7296 <vijaysantosh007@gmail.com>
2021-09-23 14:55:16 +0530

    browser updated

--- ComcastSeleniumFrm/src/test/java/com/comcast/contact/ContactTest.java
+++ ComcastSeleniumFrm/src/test/java/com/comcast/contact/ContactTest.java
@@ -5,7 +5,11 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	@Test(groups = "Smoke")
 	public void CreateContactTest(){
+		
 		System.out.println("execute create contact");
+		String BROWSER= System.getProperty("browser");
+		String ENV =System.getProperty("url");
+		System.out.println("browser"     + BROWSER +",  url"+ ENV);
 	}
 	
 	@Test(groups = "Smoke")
